52ecf0ffc7fa071888b8fda9c6c530a172110a6d
Lucadrio <iz-lucky-l@gmx.de>
2020-01-22 13:06:51 +0100

    Added some Comment-Text

--- handler.js
+++ handler.js
@@ -16,11 +16,24 @@ const response = body => ({
 const Commands = [
   {
     cmd: "/start",
-    text: "",
+    text: "🔗 Verbunden\n" +
+        "Bitte gib deinen 12-stelligen Rechnungs-Code ein.\n" +
+        "\n" +
+        "💡 Beispiel: \n" +
+        "- bzjqnlivf5dr \n" +
+        "- bzjq-nliv-fki3",
   },
   {
     cmd: "/help",
-    text: "",
+    text: "ℹ️Informationen: \n "+
+          "Gib deinen Rechnungs-Code ein und erhalte nach kurzer Zeit deinen gratis Getränk Coupon! \n" +
+          "Der Rechnungs-Code befindet sich im unteren Bereich deiner Rechnung und besteht aus 12 Zahlen und Zeichen."+
+          "Nachdem du deinen Gratiscoupon erhalten hast, bekommst du beim Vorzeigen an der Kasse ein gratis 0.25l Getränk deiner Wahl!" +
+          "\n" +
+          "📋 Weitere Informationen: \n" +
+          "- Dein Rechnungs-Code ist ab dem Kauf der Bestellung 2 Tage gültig.\n" +
+          "- Der Gratiscoupon ist ab Erstellung 1 Monat gültig. \n" +
+          "- Alle Codes sind nur ein Mal einlösbar",
   },
 ];
 
