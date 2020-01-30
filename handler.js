ee8b89c6d0dce6983e68e3743030e55ee03d81bc
Luis Meyer <luis_meyer@outlook.de>
2020-01-30 13:54:21 +0100

    Adds body parsing again

--- handler.js
+++ handler.js
@@ -40,7 +40,7 @@ module.exports.telegramBot = async ({ body }) => {
 module.exports.telegramApi = ({ body }) => {
   return new Promise(async (resolve, reject) => {
     telegram.start();
-    const { chatId, code } = body;
+    const { chatId, code } = JSON.parse(body);
 
     const apiError = async msg => {
       await telegram.editMessage(chatId, messageId, msg);
