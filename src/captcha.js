0c1d6f20432d80cc7afa6cd322f9440c96975dd9
luismeyer <luis_meyer@outlook.de>
2020-02-21 15:46:25 +0100

    Optimizes UX (#11)

--- src/captcha.js
+++ src/captcha.js
@@ -1,75 +1,117 @@
 const fetch = require("node-fetch");
 const time = require("./utils/time");
 
-const dataSiteKey = "6LfZ43cUAAAAAKTRpN6xjp9Fd7nZVDY86nUA-Zmh"
-const captchaKey = process.env.CAPTCHA_KEY
+const captchaKey = process.env.CAPTCHA_KEY;
+
+const twoCaptchaInUrl = "https://2captcha.com/in.php";
+const twoCaptchaResUrl = "https://2captcha.com/res.php";
+
+const twoCaptchaInParams = (siteKey, url) =>
+  `key=${captchaKey}&method=userrecaptcha&googlekey=${siteKey}&pageurl=${url}&json=1`;
+
+const twoCaptchaResParams = captchaId =>
+  `key=${captchaKey}&action=get&id=${captchaId}&json=1`;
+
+const startTwoCaptchaApi = (siteKey, url) =>
+  fetch(`${twoCaptchaInUrl}?${twoCaptchaInParams(siteKey, url)}`).then(res =>
+    res.json()
+  );
+
+const captchaSolvingTimeout = async statusCallback => {
+  let intervalCount = 5;
+  const interval = setInterval(async () => {
+    // notify user every second for 20 seconds
+    await statusCallback(
+      `🤖 Verbindung zum Captchaserver wird aufgebaut... ${intervalCount}%`
+    );
+    intervalCount += 5;
+  }, 1000);
+
+  await time.delay(20000);
+  clearInterval(interval);
+};
+
+const captchaSolution = captchaId =>
+  fetch(`${twoCaptchaResUrl}?${twoCaptchaResParams(captchaId)}`).then(res =>
+    res.json()
+  );
+
+const dataSiteKey = async page => {
+  const iframeHandle = await page.$("iframe");
+  const srcHandle = await iframeHandle.getProperty("src");
+  const src = await srcHandle.jsonValue();
+  return new URL(src).searchParams.get("k");
+};
 
 module.exports.solve = async (page, statusCallback) => {
   console.info("Starting captcha");
 
-  const url = page.url();
-  const res = await fetch(`https://2captcha.com/in.php?key=${captchaKey}&method=userrecaptcha&googlekey=${dataSiteKey}&pageurl=${url}&json=1`)
-    .then(res => res.json())
+  // retrive recaptcha datasiteKey from page
+  const siteKey = await dataSiteKey(page);
+  console.log("sitekey ", siteKey);
 
-  let intervalCount = 0;
-  const interval = setInterval(async () => {
-    await statusCallback(`🤖 Verbindung zum Captchaserver wird aufgebaut... ${intervalCount}%`);
-    intervalCount += 5;
-  }, 1000)
-  await time.delay(20000);
-  clearInterval(interval);
+  // request 2CaptchaApi start
+  const startApiResponse = await startTwoCaptchaApi(siteKey, page.url());
 
-  const captchaId = res.request;
-  let solution = await fetch(`https://2captcha.com/res.php?key=${captchaKey}&action=get&id=${captchaId}&json=1`).then(res => res.json());
+  // timeout for 20 seconds while the captcha gets solved
+  await captchaSolvingTimeout(statusCallback);
+  await statusCallback(`🤖 Captcha wird gelöst...`);
 
-  let retryCount = 0;
-  while (solution.request === "CAPCHA_NOT_READY" && retryCount < 30) {
+  // request 2CaptchaApi solution
+  const captchaId = startApiResponse.request;
+  let solution = await captchaSolution(captchaId);
+  console.info(solution);
+
+  // request captcha solution every 5 seconds
+  let retryCount = 1;
+  while (solution.request === "CAPCHA_NOT_READY" && retryCount <= 30) {
+    await statusCallback(`🤖 Captcha wird gelöst. Versuch: ${retryCount}/30`);
     await time.delay(5000);
-    solution = await fetch(`https://2captcha.com/res.php?key=${captchaKey}&action=get&id=${captchaId}&json=1`).then(res => res.json());
+    solution = await captchaSolution(captchaId);
     retryCount++;
-    await statusCallback(`🤖 Captcha wird gelöst: ${retryCount}/30`);
-    console.info(solution)
+    console.info(solution);
   }
 
   await statusCallback(`🤖 Captcha gelöst 🥳 warte auf Gutscheincode...`);
+
+  // executes search algorithm in client to find recaptcha callback
   return page.evaluate(token => {
     let checked = [];
-    const findCallback = (theObject) => {
-
+    const findCallback = theObject => {
       let result = null;
       if (theObject instanceof Array) {
-
         for (var i = 0; i < theObject.length; i++) {
           result = findCallback(theObject[i]);
           if (result) {
             break;
           }
         }
-
       } else {
         for (var prop in theObject) {
-
-          if (prop == 'callback') {
+          if (prop == "callback") {
             return theObject[prop];
           }
 
-          if (!checked.includes(prop) && (theObject[prop] instanceof Object || theObject[prop] instanceof Array)) {
+          if (
+            !checked.includes(prop) &&
+            (theObject[prop] instanceof Object ||
+              theObject[prop] instanceof Array)
+          ) {
             checked.push(prop);
             result = findCallback(theObject[prop]);
             if (result) {
               return result;
             }
           }
-
         }
         return;
       }
 
       return result;
-    }
+    };
 
     const callback = findCallback(___grecaptcha_cfg);
-    callback(token);
+    if (callback) callback(token);
     document.getElementById("g-recaptcha-response").innerHTML = token;
   }, solution.request);
-}
+};
